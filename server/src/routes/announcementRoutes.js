3d3913e8c29d871b9005f23cc2f012c166fc84e1
DEV <pattnaikd833@gmail.com>
2025-02-25 17:20:21 +0530

    remove unused announcement routes from the router

--- server/src/routes/announcementRoutes.js
+++ server/src/routes/announcementRoutes.js
@@ -3,22 +3,11 @@ const router = express.Router();
 
 // Import controller methods
 const {
-  createAnnouncement,
-  getAnnouncements,
-  updateAnnouncement,
-  deleteAnnouncement,
+  createAnnouncement
 } = require('../controllers/announcementController');
 
 // 1. Route to create a new announcement
 router.post('/announcements', createAnnouncement);
 
-// 2. Route to get announcements (optionally filter by audience)
-router.get('/announcements', getAnnouncements);
-
-// 3. Route to update an announcement by ID
-router.put('/announcements/:id', updateAnnouncement);
-
-// 4. Route to delete an announcement by ID
-router.delete('/announcements/:id', deleteAnnouncement);
 
 module.exports = router;
