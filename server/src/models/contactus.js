dec955acde51a63b4a187a5ad9e9c0fb66db4367
DEV <pattnaikd833@gmail.com>
2025-02-26 12:30:22 +0530

    contact us modules added

---
+++ server/src/models/contactus.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  contactEmail: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  issueRelated: {
+    type: String,
+    required: true,
+    enum: ['Billing', 'Technical Support', 'General Inquiry', 'Other'], // Modify options as needed
+    trim: true
+  },
+  message: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
