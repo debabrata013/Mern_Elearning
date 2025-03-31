2d3fdc965fa58463d011c8386e419e4002b90259
DEV <pattnaikd833@gmail.com>
2025-03-31 22:39:14 +0530

    refactor lechcher routes for video and resource management

--- server/src/routes/lechcherRoutes.js
+++ server/src/routes/lechcherRoutes.js
@@ -4,20 +4,22 @@ const express = require('express');
 const router = express.Router();
 const lechervideoController = require('../controllers/lechervideoController');
 
-// Route to get all videos
-router.get('/videos', lechervideoController.getAllVideos);
+// // Route to get all videos
+// router.get('/videos', lechervideoController.getAllVideos);
 
-// Route to get a specific video by ID
-router.get('/videos/:id', lechervideoController.getVideoById);
+// // Route to get a specific video by ID
+// router.get('/videos/:id', lechervideoController.getVideoById);
 
 // Route to create a new video
-router.post('/videos', lechervideoController.createVideo);
+router.post('/videos', lechervideoController.uploadLessonVideo);
+router.post('/resource', lechervideoController.uploadResource);
 
 // Route to update a video by ID
-router.put('/videos/:id', lechervideoController.updateVideo);
+// router.put('/videos/:id', lechervideoController.updateVideo);
 
 // Route to delete a video by ID
-router.delete('/videos/:id', lechervideoController.deleteVideo);
+router.delete('/videos/:id', lechervideoController.deleteLessonVideo);
+router.delete('/resource/:id', lechervideoController.deleteResource);
 
 module.exports = router;
 
