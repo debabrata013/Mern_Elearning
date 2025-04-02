a5e204123d07729f32fce79c1ea64ed176e893a8
DEV <pattnaikd833@gmail.com>
2025-04-02 23:27:49 +0530

    add routes for fetching and deleting user announcements

--- server/src/routes/announcementRoutes.js
+++ server/src/routes/announcementRoutes.js
@@ -3,10 +3,19 @@ const router = express.Router();
 
 // Import controller methods
 const {
-  createAnnouncement
+  createAnnouncement,
+  getUserAnnouncements,
+  deleteUserAnnouncement
+
 } = require('../controllers/announcementController');
 
 // 1. Route to create a new announcement
 router.post('/', createAnnouncement);
+router.get('/:id',getUserAnnouncements);
+router.delete('/:id/:i', deleteUserAnnouncement);
+// router.put('/:id', ubdateAnnouncement);
+
+
+
 
 module.exports = router;
