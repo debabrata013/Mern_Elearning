74456df077db71176c3a92e546cc677b98dc85d3
DEV <pattnaikd833@gmail.com>
2025-04-05 21:52:20 +0530

    button removed

--- client/src/pages/T&C.jsx
+++ client/src/pages/T&C.jsx
@@ -439,16 +439,7 @@ const TermsAndConditions = () => {
         ))}
       </div>
 
-      <div className="mt-8 text-center">
-        <label className="flex items-center justify-center">
-          <input type="checkbox" className="mr-2 h-4 w-4" />
-          <span>I have read and agree to the Terms and Conditions</span>
-        </label>
-        <button className="mt-4 bg-blue-600 hover:bg-blue-700 text-white py-2 px-8 rounded-lg">
-          Accept Terms
-        </button>
-      </div>
-
+      
       <footer className="mt-12 text-center text-gray-600 text-sm">
         <p>© {new Date().getFullYear()} AIGIRI Inc. All rights reserved.</p>
       </footer>
