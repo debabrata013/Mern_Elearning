1ae20bf5ded617a3eb77164e30a0e3264aeba1ec
DEV <pattnaikd833@gmail.com>
2025-04-09 15:42:17 +0530

    socket added

--- server/src/sockets/doubtSocket.js
+++ server/src/sockets/doubtSocket.js
@@ -1,20 +1,21 @@
 const Message = require('../models/Message');
 
+
 module.exports = (io) => {
-  io.on('connection', (socket) => {
-    console.log("Socket connected");
-
-    socket.on('join-room', ({ doubtId }) => {
-      socket.join(doubtId);
+    io.on('connection', (socket) => {
+      console.log("⚡ Socket connected");
+  
+      socket.on('join-room', ({ doubtId }) => {
+        socket.join(doubtId);
+      });
+  
+      socket.on('send-message', async ({ doubtId, sender, message }) => {
+        const msg = await Message.create({ doubt: doubtId, sender, message });
+        io.to(doubtId).emit('receive-message', msg);
+      });
+  
+      socket.on('disconnect', () => {
+        console.log("⚠️ Socket disconnected");
+      });
     });
-
-    socket.on('send-message', async ({ doubtId, sender, message }) => {
-      const newMsg = await Message.create({ doubt: doubtId, sender, message });
-      io.to(doubtId).emit('receive-message', newMsg);
-    });
-
-    socket.on('disconnect', () => {
-      console.log("Socket disconnected");
-    });
-  });
-};
+  };
