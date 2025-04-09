d4ae439721ecfd63a9b8380262830b34b589267b
DEV <pattnaikd833@gmail.com>
2025-04-09 16:19:11 +0530

    error fixed

--- server/src/routes/doubtRoutes.js
+++ server/src/routes/doubtRoutes.js
@@ -5,8 +5,8 @@ const Doubt = require('../models/Doubts');
 // Create a new doubt
 router.post('/', async (req, res) => {
   try {
-    const { courseId, studentId, title, description } = req.body;
-    const newDoubt = new Doubt({ course: courseId, student: studentId, title, description });
+    const { courseId,askedBy, title, description } = req.body;
+    const newDoubt = new Doubt({ course: courseId,  askedBy, title, description });
     await newDoubt.save();
     res.status(201).json(newDoubt);
   } catch (error) {
@@ -17,7 +17,8 @@ router.post('/', async (req, res) => {
 // Get all doubts for a course
 router.get('/course/:courseId', async (req, res) => {
   try {
-    const doubts = await Doubt.find({ course: req.params.courseId }).populate('student', 'userName email');
+    const doubts = await Doubt.find({ course: req.params.courseId }).populate('askedBy', 'userName profileImage role') // You can customize the fields
+    .populate('course', 'title');
     res.json(doubts);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -27,7 +28,7 @@ router.get('/course/:courseId', async (req, res) => {
 // Get doubts for a specific student
 router.get('/student/:studentId', async (req, res) => {
   try {
-    const doubts = await Doubt.find({ student: req.params.studentId }).populate('course');
+    const doubts = await Doubt.find({ askedBy: req.params.studentId }).populate('course');
     res.json(doubts);
   } catch (error) {
     res.status(500).json({ message: error.message });
