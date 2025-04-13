4b8a0bcd546c0af593d6a0964c5bd4f64f5c8469
DEV <pattnaikd833@gmail.com>
2025-04-13 22:21:16 +0530

    Refactor doubtRoutes: add route to retrieve a specific doubt by ID

--- server/src/routes/doubtRoutes.js
+++ server/src/routes/doubtRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Doubt = require('../models/Doubts');
+const { route } = require('./messageRoutes');
 
 // Create a new doubt
 router.post('/', async (req, res) => {
@@ -20,6 +21,17 @@ router.get('/course/:courseId', async (req, res) => {
     const doubts = await Doubt.find({ course: req.params.courseId }).populate('askedBy', 'userName profileImage role') // You can customize the fields
     .populate('course', 'title');
     res.json(doubts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const doubt = await Doubt.findById(req.params.id).populate('askedBy', 'userName profileImage role')
+    .populate('course', 'title');
+    if (!doubt) return res.status(404).json({ message: 'Doubt not found' });
+    res.json(doubt);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
